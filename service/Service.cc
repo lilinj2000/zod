8ab479ca9d8a1e170e2cac640b8bd5dfd10dafde
lilinj2000 <lilinj2000@gmail.com>
2017-09-14 15:01:43 +0800

    move proxy to zap.

--- service/Service.cc
+++ service/Service.cc
@@ -2,52 +2,52 @@
 // All rights reserved.
 
 #include "Service.hh"
-#include "Log.hh"
+#include "soil/Log.hh"
 
 namespace zod {
 
 Service::Service(SockType sock_type,
-                   const std::string& addr):
+                 const std::string& addr):
     addr_(addr),
     sock_(nullptr) {
-  ZOD_TRACE <<"Service::Service()";
+  SOIL_TRACE <<"Service::Service()";
 
   sock_ = createSock(sock_type, addr_);
   assert(sock_);
 }
 
 Service::~Service() {
-  ZOD_TRACE <<"Service::~Service()";
+  SOIL_TRACE <<"Service::~Service()";
 
   stop();
 }
 
 void Service::send(const void* msg, size_t len) {
-  ZOD_TRACE <<"Service::send()";
+  SOIL_TRACE <<"Service::send()";
 
   zmsg_t* zmsg = zmsg_new();
   zmsg_addmem(zmsg, msg, len);
 
   if (zmsg_send(&zmsg, sock_) < 0) {
-    ZOD_ERROR <<"msg send failed.\n"
+    SOIL_ERROR <<"msg send failed.\n"
               <<zmq_strerror(zmq_errno());
   }
 }
 
 void Service::send(const std::string& msg) {
-  ZOD_TRACE <<"Service::send()";
+  SOIL_TRACE <<"Service::send()";
 
   zmsg_t* zmsg = zmsg_new();
   zmsg_addstr(zmsg, msg.data());
 
   if (zmsg_send(&zmsg, sock_) < 0) {
-    ZOD_ERROR <<"msg send failed.\n"
+    SOIL_ERROR <<"msg send failed.\n"
               <<zmq_strerror(zmq_errno());
   }
 }
 
 void Service::stop() {
-  ZOD_TRACE <<"Service::stop()";
+  SOIL_TRACE <<"Service::stop()";
 
   if ( sock_ ) {
     zsock_destroy(&sock_);
